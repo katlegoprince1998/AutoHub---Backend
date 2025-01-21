4616155d0cb039111da29b5b46f115de51d2e1cb
katlegoprince1998 <princekatlego357@gmail.com>
2025-01-21 05:12:28 +0200

    Project SetUp

---
+++ user-service/src/test/java/com/katlego/mashego/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.katlego.mashego.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
